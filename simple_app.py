d6a54741fc854577bbd7767a635af2b8894c3ef2
Ikuyas <ikuyasu@gmail>
2019-03-04 12:54:04 -0600

    Add templates html with render_template()

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 # from flask import request
 
 app = Flask(__name__)
@@ -12,13 +13,16 @@ print(__name__)
 @app.route('/')
 @app.route('/<name>')
 def index(name="Treehouse"):
-    return "Hello from {}".format(name)
+    return render_template('index.html', name=name)
 
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1 + num2)
+    context = {'num1':num1, 'num2':num2}
+    return render_template('add.html', **context)
+    # ('add.html', num1=num1, num2=num2)
+    # to unpack the context
 
 app.run(debug=True, port=8000, host='0.0.0.0')
